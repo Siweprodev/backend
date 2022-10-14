b802bfeb54aacc899b5483792f35883b2a962db0
Siweprodev <sanelisiwe.sogiba@dadevs.co.za>
2022-10-14 09:46:58 +0200

    react

---
+++ Desktop/create.movie/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'John';
+  const isNameShowing = true;
+  return (
+    <div className="App">
+
+      <h1>Hello, {name}</h1>
+    </div>
+  );
+}
+
+export default App;
